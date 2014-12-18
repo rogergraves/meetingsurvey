f89d597dc1d55c106fda068adbf6cad80c5fef1a
Roger Graves <rogergraves@gmail.com>
2014-12-18 01:04:58 -0800

    Add label to leave MeetingParticipation link code blank for auto generate [#84829182]

--- app/admin/meeting_participation.rb
+++ app/admin/meeting_participation.rb
@@ -2,17 +2,15 @@ ActiveAdmin.register MeetingParticipation do
 
   permit_params :user_id, :meeting_id, :organizer, :confirmed_attendance, :link_code
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  form do |f|
+    f.inputs "Meeting Participation Fields" do
+      f.input :user
+      f.input :meeting
+      f.input :organizer
+      f.input :confirmed_attendance
+      f.input :link_code, :label => "Link code (leave blank to auto generate)"
+    end
+    f.actions
+  end
 
 end
