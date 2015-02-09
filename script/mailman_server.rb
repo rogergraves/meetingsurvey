1bb3eb7ba20a73a261eff190eaa73e9620af1b94
Roger Graves <rogergraves@gmail.com>
2015-02-08 22:39:19 -0800

    Added fields that seem to be important to puts, removed creation of Meeting object for now [#80064914]

--- script/mailman_server.rb
+++ script/mailman_server.rb
@@ -26,15 +26,23 @@ Mailman::Application.run do
         cals = Icalendar.parse(attachment.body)
         cal = cals.first
         event = cal.events.first
-        # TODO: add time_zone
-        Meeting.create!(title: event.summary.to_s,
-                        start_time: event.dtstart.to_s,
-                        end_time: event.dtend.to_s
-        )
-        # puts "summary: #{event.summary}"
-        # puts "description: #{event.description}"
-        # puts "dtstart: #{event.dtstart}"
-        # puts "dtend: #{event.dtend}"
+        # Meeting.create!(title: event.summary.to_s,
+        #                 start_time: event.dtstart.to_s,
+        #                 end_time: event.dtend.to_s
+        # )
+        puts "summary: #{event.summary}"
+        puts "description: #{event.description}"
+        puts "dtstart: #{event.dtstart}"
+        puts "dtend: #{event.dtend}"
+        puts "created: #{event.created}"
+        puts "last_modified: #{event.last_modified}"
+        puts "location: #{event.location}"
+        puts "organizer: #{event.organizer}"
+        puts "sequence: #{event.sequence}"
+        puts "status: #{event.status}"
+        puts "recurrence_id: #{event.recurrence_id}"
+        puts "rrule: #{event.rrule}" # Frequency, i.e. [#<OpenStruct frequency="WEEKLY", until=nil, count=nil, interval=nil, by_second=nil, by_minute=nil, by_hour=nil, by_day=["MO"], by_month_day=nil, by_year_day=nil, by_week_number=nil, by_month=nil, by_set_position=nil, week_start=nil>]
+        puts "attendee: #{event.attendee}"
       end
     end
   end
