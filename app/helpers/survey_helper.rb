3be4f4c83819c82a044c99d0ed5b9fa783fb2270
Michael Kasianov <kasianov.michael@gmail.com>
2015-02-13 20:51:30 +0200

    Starting to add new UI for survey [#88345874]

--- app/helpers/survey_helper.rb
+++ app/helpers/survey_helper.rb
@@ -2,17 +2,56 @@ module SurveyHelper
   def yes_or_no_survey(question, uid)
     question_id = "question_#{uid}"
     answer_id = "answer_#{uid}"
+    # NOTE: Please don't remove this %Q
+    # %Q(
+    #   <div id=#{question_id}>
+    #     #{label_tag question_id, question}
+    #     #{hidden_field_tag question_id, question}
+    #     <br>
+    #     #{radio_button_tag answer_id, 'Yes'}
+    #     #{label_tag answer_id, 'Yes', value: 'Yes'}
+    #     <br>
+    #     #{radio_button_tag answer_id, 'No'}
+    #     #{label_tag answer_id, 'No', value: 'No'}
+    #   </div>
+    # ).html_safe
+
+    #{label_tag answer_id, '<i class="fa fa-thumbs-o-up"></i>'.html_safe, value: 'Yes', class: 'btn btn-default'}
+    #{radio_button_tag answer_id, 'Yes', class: 'btn btn-default'}
+    #{radio_button_tag answer_id, 'No'}
+    #{label_tag answer_id, '<i class="fa fa-thumbs-o-down"></i>'.html_safe, value: 'No', class: 'btn btn-default'}
+
+    # %Q(
+    #   <div id=#{question_id} class='question text-center' data-toggle="buttons">
+    #     <h3>#{question}</h3>
+    #     #{hidden_field_tag question_id, question}
+    #     <br>
+    #
+    #     #{label_tag answer_id, class: 'btn btn-default' do
+    #         radio_button_tag(answer_id, 'Yes') + "<i class='fa fa-thumbs-o-up'></i>".html_safe
+    #       end}
+    #
+    #     #{label_tag answer_id, class: 'btn btn-default' do
+    #       radio_button_tag(answer_id, 'No') + "<i class='fa fa-thumbs-o-down'></i>".html_safe
+    #       end}
+    #
+    #   </div>
+    # ).html_safe
+
     %Q(
-      <div id=#{question_id}>
-        #{label_tag question_id, question}
+      <div class="question text-center">
+        <h3>#{question}</h3>
         #{hidden_field_tag question_id, question}
-        <br>
-        #{radio_button_tag answer_id, 'Yes'}
-        #{label_tag answer_id, 'Yes', value: 'Yes'}
-        <br>
-        #{radio_button_tag answer_id, 'No'}
-        #{label_tag answer_id, 'No', value: 'No'}
+        <div id=#{question_id} class="question btn-group" data-toggle="buttons">
+          <label class="btn btn-primary active">
+            <input type="radio" name="#q{answer_id}" id="#{answer_id}_yes" autocomplete="off" checked>Yes
+          </label>
+          <label class="btn btn-primary">
+            <input type="radio" name="#q{answer_id}" id="#{answer_id}_no" autocomplete="off">No
+          </label>
+        </div>
       </div>
+      <br>
     ).html_safe
   end
 
