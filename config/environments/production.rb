decdee31e4d9455b83110978e86ff734007295f8
Michael Kasianov <kasianov.michael@gmail.com>
2015-03-05 16:36:46 +0200

    Fix email sending in production  [#89705312]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV['MAILMAN_DOMAIN'],
+      user_name:            ENV['MAILMAN_USERNAME'],
+      password:             ENV['MAILMAN_PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
